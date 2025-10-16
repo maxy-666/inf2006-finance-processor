def7a229cff9f3827c5823001ce96ce9d8e45bce
maxy-666 <zmaxykeke@gmail.com>
2025-10-16 21:09:25 +0800

    security improvements to frontend, API handling

--- backend-iac/cloudfront.tf
+++ backend-iac/cloudfront.tf
@@ -1,105 +1,48 @@
-# aws_cloudfront_distribution.inf2006_distribution:
+# Defines the CloudFront distribution to serve the website content globally.
 resource "aws_cloudfront_distribution" "inf2006_distribution" {
-    aliases                         = []
-    arn                             = "arn:aws:cloudfront::335360747232:distribution/EIB2DWMEGDT19"
-    caller_reference                = "81a710ca-146b-4174-891e-d6121c39ff85"
-    continuous_deployment_policy_id = null
-    default_root_object             = null
-    domain_name                     = "d3ms0gouo9fndn.cloudfront.net"
-    enabled                         = true
-    etag                            = "E3VMBS68SDGRQH"
-    hosted_zone_id                  = "Z2FDTNDATAQYW2"
-    http_version                    = "http2"
-    id                              = "EIB2DWMEGDT19"
-    in_progress_validation_batches  = 0
-    is_ipv6_enabled                 = true
-    last_modified_time              = "2025-10-14 15:31:36.101 +0000 UTC"
-    price_class                     = "PriceClass_All"
-    retain_on_delete                = false
-    staging                         = false
-    status                          = "Deployed"
-    tags                            = {
-        "Name" = "inf2006-frontend"
-    }
-    tags_all                        = {
-        "Name" = "inf2006-frontend"
-    }
-    trusted_key_groups              = [
-        {
-            enabled = false
-            items   = []
-        },
-    ]
-    trusted_signers                 = [
-        {
-            enabled = false
-            items   = []
-        },
-    ]
-    wait_for_deployment             = true
-    web_acl_id                      = "arn:aws:wafv2:us-east-1:335360747232:global/webacl/CreatedByCloudFront-d933304e/2e563300-8157-4c3c-a8e8-d3afe2745df0"
+  enabled             = true
+  retain_on_delete    = false # Set to true in production if you want to keep the distribution when destroying the stack.
+  price_class         = "PriceClass_All"
+  
+  # The origin is the source of the files - in this case, our S3 website.
+  origin {
+    # We dynamically get the website endpoint from the S3 bucket resource.
+    domain_name = aws_s3_bucket.inf2006-s3frontend.website_endpoint
+    origin_id   = "S3-Website-inf2006-s3frontend" # A logical name for this origin.
 
-    default_cache_behavior {
-        allowed_methods            = [
-            "GET",
-            "HEAD",
-        ]
-        cache_policy_id            = "658327ea-f89d-4fab-a63d-7e88639e58f6"
-        cached_methods             = [
-            "GET",
-            "HEAD",
-        ]
-        compress                   = true
-        default_ttl                = 0
-        field_level_encryption_id  = null
-        max_ttl                    = 0
-        min_ttl                    = 0
-        origin_request_policy_id   = null
-        realtime_log_config_arn    = null
-        response_headers_policy_id = null
-        smooth_streaming           = false
-        target_origin_id           = "http://inf2006-s3frontend.s3-website-us-east-1.amazonaws.com-mgqpxtsg9ev"
-        trusted_key_groups         = []
-        trusted_signers            = []
-        viewer_protocol_policy     = "redirect-to-https"
-
-        grpc_config {
-            enabled = false
-        }
+    # This tells CloudFront that the origin is a custom web server (not just a plain S3 bucket).
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_protocol_policy   = "http-only" # S3 website endpoints use HTTP.
+      origin_ssl_protocols     = ["TLSv1.2"]
     }
+  }
 
-    origin {
-        connection_attempts      = 3
-        connection_timeout       = 10
-        domain_name              = "inf2006-s3frontend.s3-website-us-east-1.amazonaws.com"  
-        origin_access_control_id = null
-        origin_id                = "http://inf2006-s3frontend.s3-website-us-east-1.amazonaws.com-mgqpxtsg9ev"
-        origin_path              = null
+  # This defines how CloudFront handles requests from users.
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-Website-inf2006-s3frontend" # Must match the origin_id above.
+    viewer_protocol_policy = "redirect-to-https"
+    
+    # Use a standard caching policy managed by AWS for optimal performance.
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6" # CachingOptimized
+  }
 
-        custom_origin_config {
-            http_port                = 80
-            https_port               = 443
-            origin_keepalive_timeout = 5
-            origin_protocol_policy   = "https-only"
-            origin_read_timeout      = 30
-            origin_ssl_protocols     = [
-                "TLSv1.2",
-            ]
-        }
+  # Configures the SSL/TLS certificate.
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  # Defines any geographic restrictions.
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
+  }
 
-    restrictions {
-        geo_restriction {
-            locations        = []
-            restriction_type = "none"
-        }
-    }
-
-    viewer_certificate {
-        acm_certificate_arn            = null
-        cloudfront_default_certificate = true
-        iam_certificate_id             = null
-        minimum_protocol_version       = "TLSv1"
-        ssl_support_method             = null
-    }
+  tags = {
+    Name = "inf2006-frontend"
+  }
 }
--- backend-iac/s3 frontend.tf
+++ backend-iac/s3 frontend.tf
@@ -1,63 +1,34 @@
+# Defines the S3 bucket for hosting the frontend website.
 resource "aws_s3_bucket" "inf2006-s3frontend" {
-    acceleration_status         = null
-    arn                         = "arn:aws:s3:::inf2006-s3frontend"
-    bucket                      = "inf2006-s3frontend"
-    bucket_domain_name          = "inf2006-s3frontend.s3.amazonaws.com"
-    bucket_prefix               = null
-    bucket_regional_domain_name = "inf2006-s3frontend.s3.us-east-1.amazonaws.com"
-    hosted_zone_id              = "Z3AQBSTGFYJSTF"
-    id                          = "inf2006-s3frontend"
-    object_lock_enabled         = false
-    policy                      = jsonencode(
-        {
-            Statement = [
-                {
-                    Action    = "s3:GetObject"
-                    Effect    = "Allow"
-                    Principal = "*"
-                    Resource  = "arn:aws:s3:::inf2006-s3frontend/*"
-                    Sid       = "PublicReadGetObject"
-                },
-            ]
-            Version   = "2012-10-17"
-        }
-    )
-    region                      = "us-east-1"
-    request_payer               = "BucketOwner"
-    tags                        = {}
-    tags_all                    = {}
-    website_domain              = "s3-website-us-east-1.amazonaws.com"
-    website_endpoint            = "inf2006-s3frontend.s3-website-us-east-1.amazonaws.com"   
+  bucket = "inf2006-s3frontend" # The unique name for your bucket.
 
-    grant {
-        id          = "5862a41519974ff95b5bf4f641b5398fb7a3588e9755bc84760351b820abee5d"    
-        permissions = [
-            "FULL_CONTROL",
-        ]
-        type        = "CanonicalUser"
-        uri         = null
-    }
+  # This policy makes the objects in the bucket publicly readable.
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::inf2006-s3frontend/*"
+      },
+    ]
+  })
 
-    server_side_encryption_configuration {
-        rule {
-            bucket_key_enabled = true
-
-            apply_server_side_encryption_by_default {
-                kms_master_key_id = null
-                sse_algorithm     = "AES256"
-            }
-        }
-    }
-
-    versioning {
-        enabled    = false
-        mfa_delete = false
-    }
-
-    website {
-        error_document           = "error.html"
-        index_document           = "index.html"
-        redirect_all_requests_to = null
-        routing_rules            = null
-    }
+  # Configures the bucket to act as a website.
+  website {
+    index_document = "index.html"
+    error_document = "error.html" # Note: You'll need to upload an error.html file for this to work.
+  }
 }
+
+# It is best practice to manage versioning in a separate resource block.
+resource "aws_s3_bucket_versioning" "inf2006-s3frontend_versioning" {
+  bucket = aws_s3_bucket.inf2006-s3frontend.id
+  versioning_configuration {
+    # Once versioning is enabled, it can only be suspended, not disabled.
+    status = "Suspended"
+  }
+}
+
